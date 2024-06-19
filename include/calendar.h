3ab04e8089c00a0ba82b61fc7bc7249e8685fa93
Younger-Chan <lcjcm0535@126.com>
2024-06-19 17:54:58 +0800

    添加摸鱼日历以及历史上的今天

--- include/calendar.h
+++ include/calendar.h
@@ -19,18 +19,30 @@ public:
     explicit calendar(QWidget *parent = nullptr);
     ~calendar();
     void initCalendar(); // QString, QString, QString
+
 private:
     Ui::calendar *ui;
-    QNetworkAccessManager *networkCalendar;
+    QNetworkAccessManager *networkCalendar, *networkMoyu, *networkMoyuImg, *networkToday; // , *networkZhichang, *networkZhichangImg
     QString curSelectedDate;
     // int year, month, day;
     QString url;
 
 private:
 
+    void initMoyu();
+    void loadMoyu(const QString);
+    void initToday();
+    // void initZhichang();
+    // void loadZhichang(const QString);
+
 private slots:
     void onNetworkReplyCalendar(QNetworkReply *reply);
-    void on_calendarWidget_selectionChanged();
+    void onNetworkReplyMoyu(QNetworkReply *reply);
+    void onNetworkReplyMoyuImg(QNetworkReply *reply);
+    void onNetworkReplyToday(QNetworkReply *reply);
+    // void onNetworkReplyZhichang(QNetworkReply *reply);
+    // void onNetworkReplyZhichangImg(QNetworkReply *reply);
+    void calendarWidget_selectionChanged();
 };
 
 #endif // CALENDAR_H
--- source/calendar.cpp
+++ source/calendar.cpp
@@ -18,10 +18,11 @@ calendar::calendar(QWidget *parent)
     QString year = QString::number(date.year());
     QString month = QString::number(date.month());
     QString day = QString::number(date.day());
-    // qDebug() << year << " " << month << " " << day;
     QString key = "PICYEHqRx6tcAL6KkdMjU6TMiJ";
     url = QString("https://api.t1qq.com/api/tool/day/time?key=%1&y=%2&m=%3&d=%4").arg(key, year, month, day);
-    initCalendar(); // year, month, day
+    // initCalendar(); // year, month, day
+
+    connect(ui->calendarWidget, &QCalendarWidget::selectionChanged, this, &calendar::calendarWidget_selectionChanged);
 }
 
 calendar::~calendar()
@@ -29,8 +30,13 @@ calendar::~calendar()
     delete ui;
 }
 
-void calendar::on_calendarWidget_selectionChanged()
+void calendar::calendarWidget_selectionChanged()
 {
+    ui->l_date->clear();
+    ui->l_fes->clear();
+    ui->l_jq->clear();
+    ui->l_nl->clear();
+    ui->l_tgdz->clear();
     QDate date = ui->calendarWidget->selectedDate();
     QString dateStr = date.toString("yyyy/MM/dd");
     ui->l_date->setText(dateStr);
@@ -40,8 +46,8 @@ void calendar::on_calendarWidget_selectionChanged()
     QString key = "PICYEHqRx6tcAL6KkdMjU6TMiJ";
     url = QString("https://api.t1qq.com/api/tool/day/time?key=%1&y=%2&m=%3&d=%4").arg(key, year, month, day);
     initCalendar(); // year, month, day
-
 }
+
 void calendar::initCalendar() // QString y, QString m, QString d
 {
     networkCalendar = new QNetworkAccessManager(this);
@@ -50,7 +56,39 @@ void calendar::initCalendar() // QString y, QString m, QString d
     QNetworkRequest request_hl = QNetworkRequest(QUrl(url_hl));
     connect(networkCalendar, &QNetworkAccessManager::finished, this, &calendar::onNetworkReplyCalendar);
     networkCalendar->get(request_hl);
+    initMoyu();
+    initToday();
+    // initZhichang();
 }
+
+void calendar::initMoyu()
+{
+    networkMoyu = new QNetworkAccessManager(this);
+    // QString key = "PICYEHqRx6tcAL6KkdMjU6TMiJ";
+    QString url_my = "https://api.vvhan.com/api/moyu?type=json";
+    QNetworkRequest request_my = QNetworkRequest(QUrl(url_my));
+    connect(networkMoyu, &QNetworkAccessManager::finished, this, &calendar::onNetworkReplyMoyu);
+    networkMoyu->get(request_my);
+}
+
+void calendar::initToday()
+{
+    networkToday = new QNetworkAccessManager(this);
+    QString url_t = "https://api.suyanw.cn/api/today.php";
+    QNetworkRequest request_t = QNetworkRequest(QUrl(url_t));
+    connect(networkToday, &QNetworkAccessManager::finished, this, &calendar::onNetworkReplyToday);
+    networkToday->get(request_t);
+}
+/*
+void calendar::initZhichang()
+{
+    networkZhichang = new QNetworkAccessManager(this);
+    // QString key = "PICYEHqRx6tcAL6KkdMjU6TMiJ";
+    QString url_zc = "https://api.vvhan.com/api/zhichang?type=json";
+    QNetworkRequest request_zc = QNetworkRequest(QUrl(url_zc));
+    connect(networkZhichang, &QNetworkAccessManager::finished, this, &calendar::onNetworkReplyZhichang);
+    networkZhichang->get(request_zc);
+}*/
 
 void calendar::onNetworkReplyCalendar(QNetworkReply *reply)
 {
@@ -107,3 +145,138 @@ void calendar::onNetworkReplyCalendar(QNetworkReply *reply)
     reply->deleteLater();
 }
 
+void calendar::onNetworkReplyMoyu(QNetworkReply *reply)
+{
+    if (reply->error()) {
+        qDebug() << "Error:" << reply->errorString();
+        return;
+    }
+
+    // 读取响应数据
+    QByteArray response_data = reply->readAll();
+
+    // 将JSON字符串解析为QJsonDocument
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(response_data);
+
+    // 检查解析是否成功
+    if (jsonDoc.isNull()) {
+        qDebug() << "Failed to create JSON doc.";
+        return;
+    }
+    if (!jsonDoc.isObject()) {
+        qDebug() << "JSON is not an object.";
+        return;
+    }
+    QJsonObject jsonObj = jsonDoc.object();
+    QString imageUrl = jsonObj["url"].toString(); // 获取第一个图片的 URL
+    loadMoyu(imageUrl);
+}
+/*
+void calendar::onNetworkReplyZhichang(QNetworkReply *reply)
+{
+    if (reply->error()) {
+        qDebug() << "Error:" << reply->errorString();
+        return;
+    }
+
+    // 读取响应数据
+    QByteArray response_data = reply->readAll();
+
+    // 将JSON字符串解析为QJsonDocument
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(response_data);
+
+    // 检查解析是否成功
+    if (jsonDoc.isNull()) {
+        qDebug() << "Failed to create JSON doc.";
+        return;
+    }
+    if (!jsonDoc.isObject()) {
+        qDebug() << "JSON is not an object.";
+        return;
+    }
+    QJsonObject jsonObj = jsonDoc.object();
+    QString imageUrl = jsonObj["url"].toString(); // 获取第一个图片的 URL
+    loadZhichang(imageUrl);
+}*/
+
+void calendar::loadMoyu(const QString imgUrl)
+{
+    networkMoyuImg = new QNetworkAccessManager(this);
+    QNetworkRequest request_my = QNetworkRequest(QUrl(imgUrl));
+    connect(networkMoyuImg, &QNetworkAccessManager::finished, this, &calendar::onNetworkReplyMoyuImg);
+    networkMoyuImg->get(request_my);
+}
+/*
+void calendar::loadZhichang(const QString imgUrl)
+{
+    networkZhichangImg = new QNetworkAccessManager(this);
+    QNetworkRequest request_my = QNetworkRequest(QUrl(imgUrl));
+    connect(networkZhichangImg, &QNetworkAccessManager::finished, this, &calendar::onNetworkReplyZhichangImg);
+    networkZhichangImg->get(request_my);
+}*/
+
+void calendar::onNetworkReplyMoyuImg(QNetworkReply *reply)
+{
+    if (reply->error() == QNetworkReply::NoError) {
+        QPixmap pixmap;
+        pixmap.loadFromData(reply->readAll());
+        // QPixmap scaledPixmap = pixmap.scaled(ui->l_moyu->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+        // ui->l_moyu->setScaledContents(true);
+        // ui->l_moyu->setPixmap(scaledPixmap);
+        ui->l_moyu->setPixmap(pixmap);
+    } else {
+        ui->l_moyu->setText("Failed to load image.");
+    }
+    reply->deleteLater();
+}
+/*
+void calendar::onNetworkReplyZhichangImg(QNetworkReply *reply)
+{
+    if (reply->error() == QNetworkReply::NoError) {
+        QPixmap pixmap;
+        pixmap.loadFromData(reply->readAll());
+        ui->l_zc->setPixmap(pixmap);
+    } else {
+        ui->l_zc->setText("Failed to load image.");
+    }
+    reply->deleteLater();
+}*/
+
+void calendar::onNetworkReplyToday(QNetworkReply *reply)
+{
+    QVector<QString> vecToday;
+    vecToday.clear();
+    ui->lw_today->clear();
+    if (reply->error()) {
+        qDebug() << "Error:" << reply->errorString();
+        return;
+    }
+
+    // 读取响应数据
+    QByteArray response_data = reply->readAll();
+
+    // 将JSON字符串解析为QJsonDocument
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(response_data);
+
+    // 检查解析是否成功
+    if (jsonDoc.isNull()) {
+        qDebug() << "Failed to create JSON doc.";
+        return;
+    }
+    if (!jsonDoc.isObject()) {
+        qDebug() << "JSON is not an object.";
+        return;
+    }
+    QJsonObject jsonObj = jsonDoc.object();
+    QJsonObject dataObj = jsonObj["data"].toObject();
+    QJsonArray listArray = dataObj["list"].toArray();
+    for(const QJsonValue &value : listArray) {
+        QString title = value.toString();
+        vecToday << title;
+    }
+    for(int i = 0; i < vecToday.size(); i++)
+    {
+        ui->lw_today->addItem(QString::number(i+1) + "." + vecToday[i]);
+    }
+    reply->deleteLater();
+}
