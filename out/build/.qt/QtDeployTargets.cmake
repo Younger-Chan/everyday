654a3da706e709e0d2deb5026d085a0bc878a2d9
Younger-Chan <lcjcm0535@126.com>
2024-07-24 14:42:14 +0800

    移除out目录

--- out/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_everyday_FILE D:/QtWork/everyday/everyday/out/build/everyday.exe)
-set(__QT_DEPLOY_TARGET_everyday_RUNTIME_DLLS D:/Qt/6.7.0/msvc2019_64/bin/Qt6Widgetsd.dll;D:/Qt/6.7.0/msvc2019_64/bin/Qt6Xmld.dll;D:/Qt/6.7.0/msvc2019_64/bin/Qt6Multimediad.dll;D:/Qt/6.7.0/msvc2019_64/bin/Qt6Guid.dll;D:/Qt/6.7.0/msvc2019_64/bin/Qt6Networkd.dll;D:/Qt/6.7.0/msvc2019_64/bin/Qt6Cored.dll)
--- out/build/x64-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_everyday_FILE D:/QtWork/everyday/everyday/out/build/x64-Debug/everyday.exe)
-set(__QT_DEPLOY_TARGET_everyday_RUNTIME_DLLS D:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Xml.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Multimedia.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Network.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
